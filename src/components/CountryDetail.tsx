3d28130e333ce5937f7a592c3c38a147320c81b4
Roland, W. TUBONGYE <roland.beni1@gmail.com>
2022-02-27 00:56:19 +0200

    [UPDATE] change the design

--- src/components/CountryDetail.tsx
+++ src/components/CountryDetail.tsx
@@ -29,24 +29,23 @@ function CountryDetail() {
         <div>
             {!loading ?
             <div className="container px-5 py-24 mx-auto flex flex-col">
-                <div className="flex flex-col sm:flex-row mb-10">
-                    <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                <div className="flex flex-col sm:flex-row mb-10 my-24">
+                    <div className="sm:w-1/4 text-center sm:pr-8 sm:py-8">
                         <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-100 text-gray-400">
                             <img alt={`${countryCode}`} onError={addDefaultSrc}
                                  src={`https://www.worldatlas.com/r/w425/img/flag/${countryCode.toLocaleLowerCase()}-flag.jpg`}
-                                 className='w-full h-full fill-current text-primary mr-4 hidden lg:block rounded-full shadow-md'
+                                 className='w-full h-full fill-current text-primary hidden lg:block rounded-full shadow-md'
                             />
                         </div>
-                        <div className="flex flex-col items-center text-center justify-center"><h2
-                            className="font-medium title-font mt-4 text-gray-900 text-lg">{country.name || '-'}</h2>
+                        <div className="flex flex-col items-center text-center text-white justify-center">
+                            <h2 className="font-medium title-font mt-4 text-transform: uppercase text-white text-lg">{country.name || '-'}</h2>
                             <div className="w-12 h-1 bg-blue-500 rounded mt-2 mb-4"/>
                             <p className="text-base">
                                 <strong>Capital :</strong> {country.capital}
                             </p>
                         </div>
                     </div>
-                    <div
-                        className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                    <div className="sm:w-1/4 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center text-gray-100 sm:text-left">
                         <p className="leading-relaxed text-lg mb-4">
                             <strong>Continent :</strong> {country?.continent?.name}
                         </p>
@@ -60,16 +59,16 @@ function CountryDetail() {
                             <strong>Language(s) :</strong>
                             <ul>
                                 {country?.languages?.map((language: {name: string, native:string}, index: number) =>
-                                    <li key={index}>{language.name} ({language.native})</li>)}
+                                    <li className="indent-4" key={index}>{language.name} ({language.native})</li>)}
                             </ul>
                         </div>
                     </div>
-                </div>
-                <div className="lg:w-4/6 mx-auto">
-                    <div className="rounded-lg h-auto overflow-hidden">
-                        <img alt={`${countryCode}`} onError={addDefaultSrc}
-                             className="object-cover object-center h-full w-full"
-                             src={`https://www.worldatlas.com/r/w425/img/flag/${countryCode.toLocaleLowerCase()}-flag.jpg`}/>
+                    <div className="sm:w-2/6 mx-auto">
+                        <div className="rounded-lg h-auto overflow-hidden">
+                            <img alt={`${countryCode}`} onError={addDefaultSrc}
+                                 className="object-cover object-center h-full w-full"
+                                 src={`https://www.worldatlas.com/r/w425/img/flag/${countryCode.toLocaleLowerCase()}-flag.jpg`}/>
+                        </div>
                     </div>
                 </div>
             </div> :
